f7c9cf6481cc46327709dd071453f02751af391e
Arsalan127777 <sycos5036@gmail.com>
2026-07-19 23:09:37 +0500

    Update Khosa.py

--- Khosa.py
+++ Khosa.py
@@ -447,4 +447,5 @@ def login_1(uid):
                 break
             elif 'www.facebook.com' in res.get('error', {}).get('message', ''):
                print("")
-                pass
+          except:
+               pass
